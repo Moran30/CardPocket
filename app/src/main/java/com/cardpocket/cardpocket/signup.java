8854a0e44e7a608b61eb93bcab6a6271092dcd00
Moran30 <ashmoran30@gmail.com>
2015-01-24 22:34:52 +0000

    Login and Sign up page
    both pages XML and activities are linked.

--- app/src/main/java/com/cardpocket/cardpocket/signup.java
+++ app/src/main/java/com/cardpocket/cardpocket/signup.java
@@ -1,44 +1,40 @@
 package com.cardpocket.cardpocket;
 
 import android.app.Activity;
-/*import android.support.v7.app.ActionBarActivity; */
-import android.os.Bundle;
-/*import android.view.Menu; */
-/*import android.view.MenuItem;*/
+/*import android.support.v7.app.ActionBarActivity;*/
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
+/*import android.view.Menu;*/
+/*import android.view.MenuItem;*/
 
 
-public class login extends Activity {
+
+
+public class signup extends Activity{
     @Override
     public void onCreate (Bundle savedInstanceState) {
-        /* Called when the activity is first created*/
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signup);
     }
-
-
-public void bttn_signup(View view) {
-    startActivity(new Intent("com.cardpocket.cardpocket.signup"));
-}
+    public void bttn_return(View view) {
+        startActivity(new Intent("com.cardpocket.cardpocket.login"));
+    }
 }
 
-/*
-
-
-public class login extends ActionBarActivity {
+/*public class signup extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signup);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
+        getMenuInflater().inflate(R.menu.menu_signup, menu);
         return true;
     }
 
